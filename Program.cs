52ffd831d2e1d0d9f896c6e551ecf3e12d714789
Devil Queen <rominafp>
2022-03-11 14:17:10 +0330

    tamrin3

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int number;
+for (var i = 0; i < 4; i++)
+{
+    System.Console.WriteLine("hoghogh ra vared konid");
+    number=Convert.ToInt32(Console.ReadLine());
+    
+    if (number>2000000 && number<3000000)
+{
+   System.Console.WriteLine(number*0.05);
+}
+else if(number>3000000)
+{
+    System.Console.WriteLine(number*0.07);
+}
+else
+{
+    System.Console.WriteLine("maliat=0");
+}
+}
+
